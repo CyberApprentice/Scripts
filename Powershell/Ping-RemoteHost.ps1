17ae8cc14ca1c0351aa1193dd88b9b16d1ac845d
CyberApprentice <45526820+CyberApprentice@users.noreply.github.com>
2020-07-16 22:30:50 -0500

    Update

---
+++ Powershell/Ping-RemoteHost.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Test connectivity to the Internet
+.DESCRIPTION
+   The goal is have a better idea of the frequency of failed ICMP connections. It is meant to be monitored in the foreground, but can be altered to run in the background. The IP address used is 8.8.8.8 because most of the time, an Internet connection is what I'm taken. This can be manually altered, or the program can be changed to accept user input.
+.EXAMPLE
+   .\Ping-RemoteHost.ps1
+#>
+function Verb-Noun
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [int]$Param2
+    )
+
+    Begin
+    {
+        $WarningPreference = 'SilentlyContinue'# Ignore Warnings
+    }
+    Process
+    {
+        while($true)
+        {
+            start-sleep -s 1 # Pause for 1 second
+            if(!(Test-NetConnection 8.8.8.8 -InformationLevel Quiet)) # if it fails to ping the ip address
+            {
+                Write-host "Ping to 8.8.8.8 failed at $(get-date)"
+                Write-host "Waiting 5 seconds before continuing pings."
+                start-sleep -s 5
+                Write-host "Continuing pings..."
+            }
+        }
+    }
+    End
+    {
+    }
+}
---
+++ Powershell/PowerShell_Scripting_Block_Template.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Verb-Noun
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [int]$Param2
+    )
+
+    Begin
+    {
+        # The Begin block is an optional, preprocessing of the function that will only run once per call of the function. 
+        # Use this block to setup the function by initializing objects such as variables, database connections, or arrays that will be used throughout the function.  
+        # Any variables that are created in the Begin block will be accessible elsewhere in the function. 
+    }
+    Process
+    {
+        # The Process block executes on every object passed through the pipeline
+    }
+    End
+    {
+        # The End block is used after all objects have been sent through the pipeline.
+    }
+}
